1a1c0d84f11f05aeaced3c421b91f5e443305271
Adam Heiderich <adam@Adams-MacBook-Pro.local>
2020-05-17 16:54:15 +0200

    Created MVC project for creating registrations.

---
+++ RegistrationPortal/Models/RegistrationModel.cs
@@ -0,0 +1,30 @@
+
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegistrationPortal.Models
+{
+    public class RegistrationModel
+    {
+        [DisplayName("Company Name")]
+        [Required(ErrorMessage = "Company Name is required")]
+        public string CompanyName { get; set; }
+
+        [DisplayName("Contact Name")]
+        [Required(ErrorMessage = "Contact is required")]
+        public string ContactPerson { get; set; }
+
+        [DisplayName("Contact Email")]
+        [EmailAddress]
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [DisplayName("Company Address")]
+        [Required(ErrorMessage = "Address is required")]
+        public string Address { get; set; }
+
+        [DisplayName("License Key")]
+        [Required(ErrorMessage = "License Key is required")]
+        public string LicenseKey { get; set; }
+    }
+}
